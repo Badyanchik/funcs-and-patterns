2a7bed250d899de1a33ee27f4bca30c04dbcb9cc
Bohdan Perkhalyuk <bodyaperhalyuk@gmail.com>
2020-04-06 01:10:50 +0300

    Add state example

---
+++ patterns/3_behaviour/state.js
@@ -0,0 +1,75 @@
+class Light {
+  constructor (light) {
+    this.light = light;
+  }
+}
+
+class RedLight extends Light {
+  constructor () {
+    super('red');
+  }
+  sign() {
+    return 'Stop';
+  }
+}
+
+class YellowLight extends Light {
+  constructor () {
+    super('yellow');
+  }
+  sign() {
+    return 'Ready';
+  }
+}
+
+class GreenLight extends Light {
+  constructor () {
+    super('green');
+  }
+  sign() {
+    return 'Go!';
+  }
+}
+
+class TrafficLight {
+  constructor () {
+    this.states = [
+      new RedLight(),
+      new YellowLight(),
+      new GreenLight(),
+    ];
+    this.current = this.states[0];
+  }
+
+  change() {
+    const index = this.states.findIndex(light => light === this.current);
+
+    if (index + 1 < this.states.length) {
+      this.current = this.states[index + 1];
+    } else {
+      this.current = this.states[0];
+    }
+  }
+
+  sign() {
+    return this.current.sign();
+  }
+}
+
+const traffic = new TrafficLight();
+console.log(traffic.sign());
+
+traffic.change();
+console.log(traffic.sign());
+
+traffic.change();
+console.log(traffic.sign());
+
+traffic.change();
+console.log(traffic.sign());
+
+traffic.change();
+console.log(traffic.sign());
+
+traffic.change();
+console.log(traffic.sign());
