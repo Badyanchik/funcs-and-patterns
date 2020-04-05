3d5fb6885ef14f5933619b0472da3a6316e625af
Bohdan Perkhalyuk <bodyaperhalyuk@gmail.com>
2020-04-05 14:10:55 +0300

    Add adapter example

---
+++ patterns/2_structural/adapter.js
@@ -0,0 +1,33 @@
+class OldCalc  {
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case 'add': return t1 + t2;
+      case 'sub': return t1 - t2;
+      default: return NaN;
+    }
+  }
+}
+
+class NewCalc {
+  add (t1, t2) {
+    return t1 + t2;
+  }
+
+  sub (t1, t2) {
+    return t1 - t2;
+  }
+}
+
+class CalcAdapter {
+  constructor () {
+    this.calc = new NewCalc();
+  }
+
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case 'add': return this.calc.add(t1, t2);
+      case 'sub': return this.calc.sub(t1, t2);
+      default: return NaN;
+    }
+  }
+}
